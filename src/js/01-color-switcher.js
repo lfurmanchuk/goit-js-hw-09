33a6ee0489e6831649557c734b150e68d8a6c7d6
lfurmanchuk <lesyazhuruk@gmail.com>
2022-12-11 04:43:56 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
+let timeId = null;
+
+refs.startButton.addEventListener('click', onPushStart);
+refs.stopButton.addEventListener('click', onPushStop);
+
+function onPushStart(evt) {
+  refs.startButton.setAttribute('disabled', 'disabled');
+  timeId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onPushStop(evt) {
+  refs.startButton.removeAttribute('disabled');
+  clearInterval(timeId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
