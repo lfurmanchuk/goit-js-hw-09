ded57b1b1c85e5ea811739aadfb8f05f1ef200f6
lfurmanchuk <lesyazhuruk@gmail.com>
2022-12-11 04:54:31 +0200

    add task-3 and css task-2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  formInputEl: document.querySelector('.form'),
+};
+
+let timeId = null;
+
+refs.formInputEl.addEventListener('submit', onFormSubmit);
+
+/** functions */
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  clearTimeout(timeId);
+
+  const { delay, step, amount } = evt.target.elements;
+  let stepValue = Number(delay.value);
+
+  if (delay.value < 1 || step.value < 1 || amount.value < 1) {
+    Notify.failure(`All fields must be more than zero`);
+    return;
+  }
+
+  Notify.success('Please observe the promises appear below');
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, stepValue)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise #${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise #${position} in ${delay}ms`);
+      });
+    stepValue += Number(step.value);
+  }
+
+  evt.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    timeId = setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
